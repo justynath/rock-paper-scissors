194253046ace04f4c53e4e2b77fa9cb9f04bfb5b
justynath th <justynabp@gmail.com>
2024-05-14 14:58:23 +0000

    Create basic structure for the game and define conditions for winnining

--- run.py
+++ run.py
@@ -1,10 +1,22 @@
 import random
 
-options = ('rock', 'paper', 'scissors') #tuple used as these are not being changes, uses less memory than list
-users_choice = None
-computers_choice = random.choice(options)
+def play_game():
 
-users_choice = input("What's your choice? (rock, paper, scissor): ")
+    options = ('rock', 'paper', 'scissors') # tuple used as these are not being changes, uses less memory than list
+    users_choice = None
+    computers_choice = random.choice(options)
 
-print(f"User's choice: {users_choice}")
-print(f"Computer's choice: {computers_choice}")
+    while users_choice not in options:
+        users_choice = input("What's your choice? (rock, paper, scissor): ")
+
+    print(f"User's choice: {users_choice}")
+    print(f"Computer's choice: {computers_choice}")
+
+    if users_choice == computers_choice:
+        print('tie')
+    elif (users_choice == 'rock' and computers_choice == 'scissors') or (users_choice == 'paper' and computers_choice == 'rock') or (users_choice == 'scissors' and computers_choice == 'paper'):
+        print('user wins')
+    else:
+        print('computer wins')
+
+play_game()
