e259c96039a94c329a17a54b0a873b1293bb5313
justynath th <justynabp@gmail.com>
2024-05-14 16:00:13 +0000

    Add and validate name input

--- run.py
+++ run.py
@@ -1,20 +1,35 @@
 import random
 
+def users_name():
+    while True: # Never ending loop
+        try:
+            user_name = str(input("Enter your name: "))
+            if len(user_name) > 2:
+                print(f"Welcome to the game {user_name}")
+                break
+            else:
+                raise TypeError
+        except TypeError:
+            print("Name must be at least three characters")
+            continue
+
+users_name()
+
 def play_game():
 
     options = ('rock', 'paper', 'scissors') # tuple used as these are not being changes, uses less memory than list
-    users_choice = None
-    computers_choice = random.choice(options)
+    user_choice = None
+    computer_choice = random.choice(options)
 
-    while users_choice not in options:
-        users_choice = input("What's your choice? (rock, paper, scissor): ")
+    while user_choice not in options:
+        user_choice = input("What's your choice? (rock, paper, scissor): ")
 
-    print(f"User's choice: {users_choice}")
-    print(f"Computer's choice: {computers_choice}")
+    print(f"User's choice: {user_choice}")
+    print(f"Computer's choice: {computer_choice}")
 
-    if users_choice == computers_choice:
+    if user_choice == computer_choice:
         print('tie')
-    elif (users_choice == 'rock' and computers_choice == 'scissors') or (users_choice == 'paper' and computers_choice == 'rock') or (users_choice == 'scissors' and computers_choice == 'paper'):
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or (user_choice == 'paper' and computer_choice == 'rock') or (user_choice == 'scissors' and computer_choice == 'paper'):
         print('user wins')
     else:
         print('computer wins')
