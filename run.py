c707222a2d7620009bd2c6d25804ae8f6f77c7c2
justynath th <justynabp@gmail.com>
2024-05-14 14:16:53 +0000

    Set and display user and computer's choices

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 import random
+
+options = ('rock', 'paper', 'scissors') #tuple used as these are not being changes, uses less memory than list
+users_choice = None
+computers_choice = random.choice(options)
+
+users_choice = input("What's your choice? (rock, paper, scissor): ")
+
+print(f"User's choice: {users_choice}")
+print(f"Computer's choice: {computers_choice}")
