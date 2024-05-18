b1e0920b899f35150300ad196b59ce7bcedeb0f4
justynath th <justynabp@gmail.com>
2024-05-18 08:31:27 +0000

    Validate input in show_instruction function, can only be  1 or 2. Add docstrings.

--- run.py
+++ run.py
@@ -21,7 +21,12 @@ def users_name():
 user = users_name()
 
 def play_game():
-
+    """
+    Function to define the game (set number of rounds):
+    - player and computer chosing the option
+    - chioces are compared
+    - score incremented for the winner of the round
+    """
     options = ('rock', 'paper', 'scissors') # Tuple used as these are not being changes, uses less memory than list
     user_choice = None
     user_score = 0
@@ -30,7 +35,7 @@ def play_game():
     for i in range(4):
         # Validate input. Only allow values in options tupe
         while True:
-            print("Round ", i+1)
+            print("\nRound ", i+1)
             # Allow correct values with capital letters and spaces
             user_choice = input("What's your choice? (rock, paper, scissors): ").lower().strip()
             computer_choice = random.choice(options)
@@ -51,28 +56,43 @@ def play_game():
     return user_score, computer_score
 
 def show_instructions():
+    """
+    Function gives two option: 1. Read the instruction 2. Play the game
+    If option 1, two further options: play or quit
+    If option 2, game starts
+    """
     user_score = 0
     computer_score = 0
     print("1. Instructions \n2. Play the game \n")
-    instr_choice = int(input("Enter '1' to read the instructions. Enter '2' to start the game: "))
-    if instr_choice == 1:
-        f = open('instructions.txt')
-        lines = f.read()
-        f.close()
-        print(lines)
-        start_game = input("Would you like to play (y/n)?: ").lower()
-        if start_game == 'y':
-            return #play_game()
-        elif start_game == 'n':
-            sys.exit() #print("test")
-    #elif instr_choice == 2:
-        #play_game()
+    # Validate input, only allow 1 or 2
+    while True:
+        try:
+            instr_choice = int(input("Enter '1' to read the instructions. Enter '2' to start the game: "))
+            if instr_choice == 1:
+                f = open('instructions.txt')
+                lines = f.read()
+                f.close()
+                print(lines)
+                start_game = input("Would you like to play (y/n)?: ").lower()
+                if start_game == 'y':
+                    return
+                elif start_game == 'n':
+                    sys.exit() 
+            elif instr_choice == 2:
+                return
+            else:
+                raise ValueError
+        except ValueError:
+            print("Please select '1' or '2' only")
 
 show_instructions()
 user_score, computer_score = play_game()
 
 def display_score():
-    print(f"{user} = {user_score} Computer = {computer_score}")
+    """
+    Final score is displayed and the winner announced
+    """
+    print(f"\n{user} = {user_score} Computer = {computer_score}")
     if user_score > computer_score:
         print("You win!")
     elif user_score < computer_score:
